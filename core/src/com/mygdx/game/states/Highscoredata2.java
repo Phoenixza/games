b54edda35f8b9476d185c0e5a166d03d29ae51fa
Michael Welser <m.welser@gmx.net>
2015-08-07 11:36:06 +0200

    Create Highscoredata2.java

---
+++ core/src/com/mygdx/game/states/Highscoredata2.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+
+/**
+ * Created by welser on 06.08.2015.
+ */
+public class Highscoredata implements Json.Serializable {
+
+    public String points;
+    public String name;

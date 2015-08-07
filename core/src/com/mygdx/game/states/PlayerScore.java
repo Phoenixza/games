5f1ef8031dd1db984d009f1acd7d6f46a911f9f5
Michael Welser <m.welser@gmx.net>
2015-08-07 09:00:10 +0200

    Update PlayerScore.java

--- core/src/com/mygdx/game/states/PlayerScore.java
+++ core/src/com/mygdx/game/states/PlayerScore.java
@@ -7,3 +7,16 @@ public class PlayerScore {
 
     String name;
     double points;
+
+    public PlayerScore(double v, String user) {
+        this.name = user;
+
+        this.points = v;
+    }
+
+
+    public void PlayScore( double points, String name){
+        this.name = name;
+        this.points = points;
+    }
+}
